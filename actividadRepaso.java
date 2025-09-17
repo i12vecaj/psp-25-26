752927d5017dcc637c47399d1b7e8d3e3dd82398
Jonathan Martínez <11787234+its-johnny@users.noreply.github.com>
2025-09-17 20:40:47 +0200

    Actividad de repaso del primer día
    Dentro del archivo he especificado como se subiría en condiciones usando Git

---
+++ actividadRepaso.java
@@ -0,0 +1,46 @@
+
+/*
+ * proceso para añadir este archivo desde Git (no he podido porque uso SSH)
+ * primeramente abririamos CMD en Windows o Bash en Linux
+ * clonariamos el repo con git clone https://github.com/i12vecaj/psp-25-26.git
+ * git branch -c dev_16
+ * git switch dev_16
+ * pegaría el archivito en algun lado
+ * git add .
+ * git commit -m "Actividad clase" 
+ * git push upstream o git push origin
+ * ya debería estar 
+ */
+
+import java.util.*; // importar librería típica
+
+class main {
+
+    public static void main(String[] args) {
+
+        Scanner scannerNombre = new Scanner(System.in);
+        System.out.println("Bienvenido! Introduce tu nombre");
+
+        String nombre = scannerNombre.nextLine();
+        System.out.println("Perfecto " + nombre + ", ahora introduce tu edad");
+
+        Scanner scannerEdad = new Scanner(System.in);
+        int edad = scannerEdad.nextInt(); // importante que aquí el scanner espere un int y no un string
+        int edad100 = (100 - edad) + 2025; // aquí estamos suponiendo que estamos en 2025
+
+
+        System.out.println("Que sepas mi pequeño " + nombre + " que cumplirás 100 años en el año " + edad100);
+
+        /* 
+        esta parte la he mirado pq me preguntaba cómo 
+        se podría hacer el programa en condiciones
+        si añadimos
+        import java.time.*;
+    
+        y para la edad usamos esta linea
+        int edad100 = (100 - edad) + Year.now().getValue();
+
+        el resultado sería mejor debido a que no importaría la fecha
+        */
+    }
+}
