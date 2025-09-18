291a8cda67892f9512e9c112f2ea3c4ee9745c0d
Dany26G <groelio26@gmail.com>
2025-09-18 12:41:33 +0200

    Ejercicio de Nombre y edad
    Simple codigo java donde se le pide el nombre y la edad al usuario

---
+++ dev_5/Ejercicio de Nombre y edad/src/NombreEdad.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class NombreEdad {
+
+	public String nombre;
+	public int edad;
+	
+	public NombreEdad(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner s =  new Scanner(System.in);
+		NombreEdad nuevo = new NombreEdad("q",0);
+		
+		System.out.println("Introduzca su nombre: ");
+		String Nomb = s.nextLine();
+		nuevo.setNombre(Nomb);
+		while(true) {
+		System.out.println("Introduzca su edad: ");
+		if (s.hasNextInt()) {
+			ed = s.nextInt();
+			nuevo.setEdad(ed);
+			break;
+		}
+		else {
+			System.out.println("Se requiere un numero exacto de años.");
+        s.next();
+		}}
+
+		
+		
+		System.out.println("Hola "+ nuevo.getNombre()+ ", tu edad es "+ nuevo.getEdad() + " años.");
+
+	}
+
+}
