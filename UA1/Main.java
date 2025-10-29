1a6b16e2a23caf1bf6af2c5903463a1456241ae6
Dany26G <groelio26@gmail.com>
2025-10-29 20:13:19 +0100

    Add files via upload

--- UA1/Main.java
+++ UA1/Main.java
@@ -3,43 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Tarea tarea1 = new Tarea();
-        Tarea tarea2 = new Tarea();
-        Tarea tarea3 = new Tarea();
+        LavarRopa lavarRopa = new LavarRopa();
+        Cocinar cocinar = new Cocinar();
+        Limpiar limpiar = new Limpiar();
 
-        Thread t1 = new Thread(tarea1){
-            @Override
-            public void run() {System.out.println("Tarea 1 ejecutándose en el hilo 1");}
-        };
+        Thread lr = new Thread(lavarRopa);
+        Thread cc = new Thread(cocinar);
+        Thread ll = new Thread(limpiar);
 
-        Thread t2 = new Thread(tarea2){
-            @Override
-            public void run() {System.out.println("Tarea 2 ejecutándose en el hilo 2");}
-        };
-
-        Thread t3 = new Thread(tarea3){
-            @Override
-            public void run() {System.out.println("Tarea 3 ejecutándose en el hilo 3");}
-        };
-
-
-       t1.start();
-       t2.start();
-       t3.start();
-
-        try {
-            t1.sleep(500);
-            System.out.println("Tarea 1 finalizada.");
-            t2.sleep(500);
-            System.out.println("Tarea 2 finalizada.");
-            t3.sleep(500);
-            System.out.println("Tarea 3 finalizada.");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        //El proceso es la ejecucion de las tareas y funciones del codigo siguiendo un orden establecido.
-        //El hilo es es un ayudante que nos permite ejecutar el proceso cuando se lo pidamos, con lo que le pidamos y reduciendo nuestra carga de trabajo total.
-
+        lr.start();
+        cc.start();
+        ll.start();
     }
 }
