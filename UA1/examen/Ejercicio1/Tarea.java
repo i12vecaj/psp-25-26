257ee67b55d165a7ee4e942e13f6f9ead5d80cfd
Davpp24 <pelaezperezdavid@gmail.com>
2025-10-29 19:04:02 +0100

    encabezado interfaz

--- UA1/examen/Ejercicio1/Tarea.java
+++ UA1/examen/Ejercicio1/Tarea.java
@@ -1,3 +1,10 @@
+/*
+ * Nombre: David Peláez Pérez
+ * Fecha: 29/20/2025
+ * Descripción: [Crear un interfaz para que se puedan crear hilos con nombre y que a la hora de ejectarse muestren su nombre en que hilo estan 
+ * y a la hora de terminar muestren que han finalizado]
+ * FR implementados: [FR1, FR2...]
+ */
 public class Tarea implements Runnable {
     public String nombre;
 
