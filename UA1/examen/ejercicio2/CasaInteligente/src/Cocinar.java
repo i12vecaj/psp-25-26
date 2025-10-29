ff85c3a31194008447e55bb409c7be70a753b6b5
Bernardo Cubero <berniecubero@gmail.com>
2025-10-29 19:59:08 +0100

    Ejercicio 2 Examen

---
+++ UA1/examen/ejercicio2/CasaInteligente/src/Cocinar.java
@@ -0,0 +1,2 @@
+public class Cocinar {
+}
---
+++ UA1/examen/ejercicio2/CasaInteligente/src/Main.java
@@ -0,0 +1,28 @@
+ /*
+
+  * Nombre: Bernardo Cubero
+
+  * Fecha: 29/10/2025
+
+  * Descripción: Tareas de una casa inteligente
+
+  * FR implementados:
+   FR1:
+   FR2:
+   FR3:
+   FR4:
+   FR5:
+   FR6:
+  */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
