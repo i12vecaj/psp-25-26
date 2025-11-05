d102a864ff34494a906bfc9e62f5c29676130192
miguelvaro1806 <flokivaro@gmail.com>
2025-11-05 21:43:49 +0100

    tarea 1 UA 2

---
+++ UA2/tareas/dev_30/ua2tarea1fr1.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+public class ua2tarea1fr1 {
+    static int contador = 0;
+    public static void main(String[] args) {
+        Thread[] hilos = new Thread[5];
+
+        for (int i = 0; i < 5; i++) {
+            hilos[i] = new MiHilo("Hilo-" + (i + 1));
+            hilos[i].start();
+        }
+
+        System.out.println("Valor final del contador: " + contador);
+        System.out.println("Valor esperado: 5000");
+    }
+
+    static class MiHilo extends Thread {
+        static final Random random = new Random();
+        public MiHilo(String nombre) {
+            super(nombre);
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 1000; i++) {
+                contador++;
+            }
+        }
+    }
+}
---
+++ UA2/tareas/dev_30/ua2tarea1fr2.java
@@ -0,0 +1,43 @@
+public class ua2tarea1fr2 {
+    static int contador = 0;
+
+    public static synchronized void incrementar() {// usando Synchronized se evita que los hilos se solapen
+        contador++;
+    }
+
+    public static void main(String[] args) {
+        Thread[] hilos = new Thread[5];
+
+        // Crear y lanzar los hilos
+        for (int i = 0; i < 5; i++) {
+            hilos[i] = new MiHilo("Hilo-" + (i + 1));
+            hilos[i].start();
+        }
+
+        // con join esperamos a que terminen todos los hilos
+        for (int i = 0; i < 5; i++) {
+            try {
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        // Resultado final
+        System.out.println("Valor final del contador: " + contador);
+        System.out.println("Valor esperado: 5000");
+    }
+
+    static class MiHilo extends Thread {
+        public MiHilo(String nombre) {
+            super(nombre);
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                ua2tarea1fr2.incrementar(); // llamada sincronizada
+            }
+        }
+    }
+}
