fc0bb61da0a78c37fa3f329db910e43bf59ef65a
Dany26G <groelio26@gmail.com>
2025-11-12 19:51:55 +0100

    Update ua2tarea2.java

--- UA2/tareas/dev_0/tarea_2/ua2tarea2.java
+++ UA2/tareas/dev_0/tarea_2/ua2tarea2.java
@@ -1,5 +1,54 @@
-package UA2.tareas.dev_0.tarea_2;
+import java.util.Random;
 
-public class ua2tarea2 {
+public class CuentaCorriente {
 
+    private double saldo;
+    private Random random = new Random();
+
+    public CuentaCorriente(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public synchronized double getSaldo() {
+        dormirAleatorio();
+        return saldo;
+    }
+
+    public synchronized void setSaldo(double nuevoSaldo) {
+        dormirAleatorio();
+        this.saldo = nuevoSaldo;
+    }
+
+    public synchronized void ingresar(double cantidad, String nombrePersona) {
+        System.out.println(nombrePersona + " va a ingresar " + cantidad + "€...");
+        System.out.println("Saldo anterior: " + saldo + "€");
+
+        dormirAleatorio();
+
+        saldo += cantidad;
+
+        System.out.println("Saldo final tras ingreso de " + nombrePersona + ": " + saldo + "€\n");
+    }
+
+    private void dormirAleatorio() {
+        try {
+            int tiempo = 250 + random.nextInt(1751); // entre 250 y 2000 ms
+            Thread.sleep(tiempo);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("El hilo fue interrumpido.");
+        }
+    }
+
+    public static void main(String[] args) {
+        CuentaCorriente cuenta = new CuentaCorriente(1000.0);
+
+        Thread t1 = new Thread(() -> cuenta.ingresar(500, "Ana"));
+        Thread t2 = new Thread(() -> cuenta.ingresar(300, "Luis"));
+        Thread t3 = new Thread(() -> cuenta.ingresar(200, "Marta"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
