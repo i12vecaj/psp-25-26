4b7b7177b6831bc741d7198f63a8603675e97bf7
Elena Expósito <elexla97@gmail.com>
2025-11-12 20:51:33 +0100

    Add 'T2 - T2 from UA2

---
+++ UA2/tareas/dev_11/tarea_2/HiloIngreso.java
@@ -0,0 +1,23 @@
+/**
+ * Clase que representa un hilo encargado de realizar un ingreso en una cuenta corriente.
+ */
+public class HiloIngreso extends Thread {
+    private final CuentaCorriente cuenta;
+    private final double cantidad;
+
+    public HiloIngreso(CuentaCorriente cuenta, double cantidad, String nombre) {
+        super(nombre); // Nombre del hilo
+        this.cuenta = cuenta; // Objeto CuentaCorriente compartido
+        this.cantidad = cantidad; // Cantidad a ingresar
+    }
+
+    @Override
+    public void run() { // Ejecuta el ingreso en la cuenta
+        try {
+            cuenta.ingresar(cantidad, getName());
+        } catch (Exception e) {
+            System.err.println("Error en " + getName() + ": " + e.getMessage());
+        }
+    }
+}
+
