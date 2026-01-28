510ebe7c1c6342f5f7ae95837574f49455891d5a
Cerezo <manucgr09@gmail.com>
2026-01-28 18:58:50 +0100

    Examen

---
+++ UA3/examen/dev_07/HiloServidorChat.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.net.*;
+
+public class HiloServidorChat extends Thread {
+	DataInputStream fentrada;
+	Socket socket = null;
+	ComunHilos comun;
+
+	public HiloServidorChat(Socket s, ComunHilos comun) {
+		this.socket = s;
+		this.comun = comun;
+		try {
+			// CREO FLUJO DE entrada para leer los mensajes
+			fentrada = new DataInputStream(socket.getInputStream()); // flujo de entrada
+		} catch (IOException e) {
+			System.out.println("ERROR DE E/S");
+			e.printStackTrace();
+		}
+	}// ..
+
+	public void run() {
+		System.out.println("NUMERO DE CONEXIONES ACTUALES: " + comun.getACTUALES());
+
+		// NADA MAS CONECTARSE LE ENVIO TODOS LOS MENSAJES
+		String texto = comun.getMensajes();
+		EnviarMensajesaTodos(texto);
+
+		while (true) {
+			String cadena = "";
+			try {
+				cadena = fentrada.readUTF();
+				if (cadena.trim().equals("*")) {// EL CLIENTE SE DESCONECTA
+					comun.setACTUALES(comun.getACTUALES() - 1);
+					System.out.println("NUMERO DE CONEXIONES ACTUALES: " + comun.getACTUALES());
+				}
+
+				comun.setUltimo(cadena);
+				EnviarMensajesaTodos(comun.getUltimo());//nos da el ultimo envio de mensajes(esta funcion esta definida en
+														//comunHilos)
+
+			} catch (Exception e) {// EL CLIENTE SE DESCONECTA A LO BRUTO Ctrl+C o similar
+				comun.setACTUALES(comun.getACTUALES() - 1);
+				System.out.println("NUMERO DE CONEXIONES ACTUALES: " + comun.getACTUALES());
+				System.out.println("ERROR: " + e.getMessage());
+				break;
+			}
+		} // fin while
+
+		// se cierra el socket del cliente
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}// run
+
+	// ENVIA LOS MENSAJES DEL CHAT A LOS CLIENTES
+	private void EnviarMensajesaTodos(String texto) { // se pone el string para enviar el texto recivido
+		int i;										  //(esta declarado en ClienteChat en el run)
+		// recorremos tabla de sockets para enviarles los mensajes
+		for (i = 0; i < comun.getCONEXIONES(); i++) {
+			Socket s1 = comun.getElementoTabla(i);
+			if (!s1.isClosed()) {
+				try {
+					DataOutputStream fsalida = new DataOutputStream(s1.getOutputStream());
+					fsalida.writeUTF(texto);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} // for
+
+	}// EnviarMensajesaTodos
+
+}// ..HiloServidorChat
